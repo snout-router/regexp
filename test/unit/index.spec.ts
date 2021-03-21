97dff73c389f0187c50877ad40bff243ce501e2f
Erin Millard <ezzatron@gmail.com>
2021-03-21 16:01:36 +1000

    Fix incorrect test description

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -2,7 +2,7 @@ import * as index from '../../src/index'
 import {escape} from '../../src/main'
 
 describe('module index', () => {
-  it('should provide access to param()', () => {
+  it('should provide access to escape()', () => {
     expect(index.escape).toBe(escape)
   })
 })
