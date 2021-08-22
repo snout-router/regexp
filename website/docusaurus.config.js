4e40c68ad876258d97727c143e47ec04e7702664
Erin Millard <ezzatron@gmail.com>
2021-08-22 11:50:26 +1000

    Explicitly specify primary API entry

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,6 +1,7 @@
 const {createConfig} = require('@snout/docusaurus-config')
 
 module.exports = createConfig({
+  primaryApiEntry: 'escape',
   rootPath: __dirname,
   title: 'Snout RegExp Utilities',
 })
