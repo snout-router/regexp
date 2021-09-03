ee94627c505adf5dee7f792394ae75424005202f
Erin Millard <ezzatron@gmail.com>
2021-09-03 16:10:28 +1000

    Upgrade Docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,7 +1,30 @@
 const {createConfig} = require('@snout/docusaurus-config')
 
 module.exports = createConfig({
-  primaryApiEntry: 'escape',
   rootPath: __dirname,
   title: 'Snout RegExp Utilities',
+  redirects: [
+    {
+      from: '/api',
+      to: '/api/escape',
+    },
+  ],
+  navbarItems: [
+    {
+      to: 'api/escape',
+      label: 'API',
+      position: 'right',
+    },
+  ],
+  footerLinks: [
+    {
+      title: 'Docs',
+      items: [
+        {
+          label: 'API reference',
+          to: 'api/escape',
+        },
+      ],
+    },
+  ],
 })
